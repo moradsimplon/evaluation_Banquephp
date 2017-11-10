34425a32ea6520f6f632d204210f0c58df2b0d27
moradsimplon <morad.bzioui@laposte.net>
2017-11-10 16:22:04 +0100

    add getters and setters in Account.php

--- controllers/index.php
+++ controllers/index.php
@@ -1,17 +1,22 @@
 <?php
 require 'model/bddconnect.php';
+
 function chargerClasse($classe)
 {
     if ($classe == 'AccountManager') {
 
-        require 'Model/' . $classe . '.php';
+        require 'model/' . $classe . '.php';
     } else {
 
-        require 'Model/entities/' . $classe . '.php'; // On inclut la classe correspondante au paramètre passé.
+        require 'entities/' . $classe . '.php'; // On inclut la classe correspondante au paramètre passé.
     }
 }
 
 spl_autoload_register('chargerClasse'); // On enregistre la fonction en autoload pour qu'elle soit appelée dès qu'on instanciera une classe non déclarée.
+
+$manager = new AccountManager($bdd);
+
+$accounts = $manager->getAccounts();
 
 
 include "views/indexVue.php";
